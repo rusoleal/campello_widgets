38204df6591defa6749e3ccf7bc1c95fadff1b5e
rusoleal <rusoleal@gmail.com>
2026-04-23 23:14:17 +0200

    android ci fix: derive NDK path from toolchain + explicit CI variable

--- android.cmake
+++ android.cmake
@@ -17,14 +17,23 @@ if(ANDROID_NDK)
     set(ANDROID_NDK_PATH ${ANDROID_NDK})
 elseif(CMAKE_ANDROID_NDK)
     set(ANDROID_NDK_PATH ${CMAKE_ANDROID_NDK})
+elseif(ANDROID_NDK_HOME)
+    set(ANDROID_NDK_PATH ${ANDROID_NDK_HOME})
 elseif(DEFINED ENV{ANDROID_NDK_HOME})
     set(ANDROID_NDK_PATH $ENV{ANDROID_NDK_HOME})
+elseif(CMAKE_TOOLCHAIN_FILE)
+    # Derive NDK root from toolchain path: <ndk>/build/cmake/android.toolchain.cmake
+    get_filename_component(_toolchain_dir ${CMAKE_TOOLCHAIN_FILE} DIRECTORY)
+    get_filename_component(ANDROID_NDK_PATH ${_toolchain_dir}/../../.. ABSOLUTE)
 endif()
 
 if(ANDROID_NDK_PATH)
     target_include_directories(campello_widgets PRIVATE
         ${ANDROID_NDK_PATH}/sources/android/native_app_glue
     )
+    message(STATUS "Android NDK native_app_glue include: ${ANDROID_NDK_PATH}/sources/android/native_app_glue")
+else()
+    message(WARNING "Could not locate Android NDK path for native_app_glue includes")
 endif()
 
 target_link_libraries(campello_widgets
